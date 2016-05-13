48dbf79d6bf0e0f81314ca364dd229438b75235f
ponycoder <571533442@qq.com>
2016-05-13 21:45:56 +0800

    ss

--- PonyFramework-Swift/PonyFramework-Swift/MyFramework.swift
+++ PonyFramework-Swift/PonyFramework-Swift/MyFramework.swift
@@ -11,20 +11,4 @@ import UIKit
 public class MyFramework: NSObject {
     static let bundle = NSBundle(forClass: MyFramework.self)
     static let MyFrameworkStr = "MyFramework"
-    
-    public static func loadVCFromStoryboard() {
-        let vc =  UIStoryboard(name: "MyFramework", bundle: bundle).instantiateViewControllerWithIdentifier("MyFrameworkViewController")
-        UIApplication.sharedApplication().keyWindow?.rootViewController?.presentViewController(vc, animated: true, completion: nil
-        )
-    }
-    
-    public static func openVCFromXib(){
-        let vc = MyFrameworkViewController(nibName:MyFrameworkStr,bundle:bundle)
-        UIApplication.sharedApplication().keyWindow?.rootViewController?.presentViewController(vc, animated: true, completion: nil)
-    }
-    
-    public static func loadImage() -> UIImage {
-        let image = UIImage(named: MyFrameworkStr + ".png",inBundle: bundle,compatibleWithTraitCollection:nil)
-        return image!
-    }
 }
